69f7388a14dd7154525f3c878e4a585cb460d966
swalker <solutionwalker0319@gmail.com>
2022-12-15 23:50:06 +0900

    init commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import bakjoon.Sample;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world! " + new Sample().Solution());
+
+    }
+}
---
+++ src/bakjoon/Sample.java
@@ -0,0 +1,12 @@
+package bakjoon;
+
+public class Sample
+{
+
+    public int Solution()
+    {
+        return -1;
+
+    }
+
+}
